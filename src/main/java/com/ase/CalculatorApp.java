96553550e3279d671cad762efae3bcb0dde08165
jayavardhanpatil <jayawardhan.patil@gmail.com>
2019-10-25 16:01:10 -0700

    Demo classes
    Demo classes to test CI

--- src/main/java/com/ase/CalculatorApp.java
+++ src/main/java/com/ase/CalculatorApp.java
@@ -4,4 +4,9 @@ package com.ase;
  * Created by jayavardhanpatil on 10/25/19
  */
 public class CalculatorApp {
+
+    public static void main(String[] args) {
+        System.out.println("test CI CD Demo");
+    }
+
 }
