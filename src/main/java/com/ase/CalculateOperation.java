7214689e55310efa5803c8df59658b8a0d7455e7
jayavardhanpatil <jayawardhan.patil@gmail.com>
2019-10-25 18:28:06 -0700

    Added Calculate Operation Interface

---
+++ src/main/java/com/ase/CalculateOperation.java
@@ -0,0 +1,10 @@
+package com.ase;
+
+/**
+ * Created by jayavardhanpatil on 10/25/19
+ */
+
+public interface CalculateOperation {
+
+    Double calculateResult(double firstOperand, double secondOperand);
+}
