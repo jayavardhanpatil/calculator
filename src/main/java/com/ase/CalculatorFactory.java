0870b564d168b1d645949135e2f6ff6d2c508da2
jayavardhanpatil <jayawardhan.patil@gmail.com>
2019-10-25 18:30:10 -0700

    Calculatefactory added

---
+++ src/main/java/com/ase/CalculatorFactory.java
@@ -0,0 +1,29 @@
+package com.ase;
+
+/**
+ * Created by jayavardhanpatil on 10/25/19
+ */
+
+public abstract class CalculatorFactory {
+
+    private final char ADDITION = '+';
+    private final char SUBSTRACTION = '-';
+    private final char MULTIPLICATION = '*';
+    private final char DIVISION = '/';
+
+    public CalculateOperation getCalculateMehod(char operation){
+
+        if(operation == ADDITION){
+            return new Addition();
+        }else if(operation == SUBSTRACTION){
+            return new Substraction();
+        }else if(operation == MULTIPLICATION){
+            return new Multipilcation();
+        }else if(operation == DIVISION){
+            return new Division();
+        }
+
+        return null;
+    }
+
+}
